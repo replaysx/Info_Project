734da475e70c0769e147312025a7de7e4cb80545
replaysx <fa.beckering@icloud.com>
2023-04-25 17:27:43 +0200

    Objects
    Signed-off-by: replaysx <fa.beckering@icloud.com>

---
+++ src/object/Key.java
@@ -0,0 +1,18 @@
+package object;
+
+
+import javax.imageio.ImageIO;
+import java.io.IOException;
+
+public class Key extends  SuperObject{
+    public Key(){
+        name = "Key";
+
+        try {
+            image = ImageIO.read(getClass().getResourceAsStream("/objects#/pixil-frame-0(2).png"));
+        }
+        catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/object/SuperObject.java
@@ -0,0 +1,19 @@
+package object;
+
+import Main.GamePanel;
+
+import java.awt.*;
+import java.awt.image.BufferedImage;
+
+public class SuperObject {
+    public BufferedImage image;
+    public  String name;
+    public boolean collision = false;
+    public int worldX, worldY;
+
+    public void draw(Graphics2D g2, GamePanel gp){
+        int screenX = worldX - gp.player.worldX + gp.player.screenX;
+        int screenY = worldY - gp.player.worldY + gp.player.screenY;
+        g2.drawImage(image,screenX,screenY,gp.tileSize, gp.tileSize, null);
+    }
+}
